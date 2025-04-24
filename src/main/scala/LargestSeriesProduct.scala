2ff4465d4b350ca22ee1d1d81697d4da3a338d3e
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-24 13:55:47 +0200

    refactor

--- src/main/scala/LargestSeriesProduct.scala
+++ src/main/scala/LargestSeriesProduct.scala
@@ -1,4 +1,4 @@
 object Series:
    def largestProduct(span: Int, digits: String): Option[Int] =
-      if span > digits.length || !digits.forall(_.isDigit) || span < 0 then None
-      else digits.sliding(span).map(value => value.toList.map(_.toString.toInt).product).maxOption
+      if span <= 0 || span > digits.length || !digits.forall(_.isDigit) then None
+      else digits.sliding(span).map(_.map(_.asDigit).product).maxOption
